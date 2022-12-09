fe44e76c85dd291a7eec8a6cdd21c07d7813f6e0
Samuel Koomson <kskoomson1@gmail.com>
2022-12-09 15:47:36 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+include < stdio.h >
+/**
+ * main - prints the alphabet in lowercase, except for qand e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if (letter != 'e' && letter != 'q')
+		putchar(letter);
+}
+putchar('\n');
+return (0);
+}
