f2440ef957c827700288b328efe49339b96ed5ee
Samuel Koomson <kskoomson1@gmail.com>
2022-12-09 17:22:25 -0800

    printing numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,11 @@ include < stdio.h >
  */
 int main(void)
 {
-	char letter;
+char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-	if (letter != 'e' && letter != 'q')
-		putchar(letter);
+if (letter != 'e' && letter != 'q')
+putchar(letter);
 }
 putchar('\n');
 return (0);
