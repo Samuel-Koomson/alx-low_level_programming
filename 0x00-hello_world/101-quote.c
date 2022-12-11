166c9d6e48ee686946b53d0c00080ce60ce04d1f
Samuel Koomson <kskoomson1@gmail.com>
2022-12-11 11:03:57 -0800

    piece of art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - print a string that displays and that piece of art is useful
  *
- * Description: using the main function
- *
- * this program prints and that piece of art is useful-Dora Korpar,
- *
- * Return (1)
+ * Description: this program prints and that piece of art
+ * Return: Always (1) success
  */
+
 int main(void)
 {
-	fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n");
-	return (1);
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, s, 59);
+return (1);
 }
