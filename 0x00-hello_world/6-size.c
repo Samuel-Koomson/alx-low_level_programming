9f29bcb0fb252c22e9cefdd055d589bc6506bf92
Samuel Koomson <kskoomson1@gmail.com>
2022-12-11 09:59:57 -0800

    rsolving 32 and 64 bits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of char: %ld bytes\n", sizeof(c));
-	printf("Size of int: %ld bytes\n", sizeof(i));
-	printf("Size of long int: %ld bytes\n", sizeof(li));
-	printf("Size of long long int: %ld bytes\n", sizeof(lli));
-	printf("Size of float %ld bytes\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float %ld byte(s)\n", sizeof(f));
 	return (0);
 }
