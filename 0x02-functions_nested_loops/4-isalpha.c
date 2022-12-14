e72d066502cbb029c3fcdbfab97116e859e0b164
Samuel Koomson <kskoomson1@gmail.com>
2022-12-14 08:03:21 -0800

    checking for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - checks if data is alphabetic or not
+ * @c: takes character for analysis
+ * Return: returns value
+ */
+
+int _isalpha(int c)
+{
+	int value;
+
+	if (isalpha(c))
+	{
+		value = 1;
+	}
+	else
+	{
+		value = 0;
+	}
+	return (value);
+}
+
+
