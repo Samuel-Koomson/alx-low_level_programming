01034656bfba8a0be97a8009b9a56fdd14891c8e
Samuel Koomson <kskoomson1@gmail.com>
2022-12-14 01:41:49 -0800

    printing alphabets in 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - printing alphabet x10
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char alpha_x10;
+
+	for (alpha_x10 'a'; alpha_x10 <= 'z'; alpha_x10++ * 10)
+	{
+		_putchar(alpha_x10);
+	}
+	_putchar('\n');
+}
