84d1ac05a616bafb50ee8d249db368f5a9bdf236
Samuel Koomson <kskoomson1@gmail.com>
2022-12-14 06:59:26 -0800

    checking for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include <ctype.h>
+
+/**
+ * _islower - checks for lower data sets
+ * @c: accepts inputs
+ * Return: returns status code
+ */
+
+int _islower(int c)
+
+{
+int status_case;
+
+if (_islower(c))
+{
+status_case = 1;
+}
+else
+{
+status_case = 0;
+}
+return (status_case);
+}
