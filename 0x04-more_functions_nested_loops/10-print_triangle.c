fde59bab14a565a265bcddd5e4610e9ef2efa78d
Samuel Koomson <kskoomson1@gmail.com>
2022-12-15 15:44:53 -0800

    printing triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print triangles using #.
+ * @size: size of triangle.
+ */
+void print_triangle(int size)
+{
+int hash, index;
+
+if (size > 0)
+{
+for (hash = 1; hash <= size; hash++)
+{
+for (index = size - hash; index > 0; index--)
+_putchar(' ');
+
+for (index = 0; index < hash; index++)
+_putchar('#');
+
+if (hash == size)
+continue;
+
+_putchar('\n');
+}
+}
+
+_putchar('\n');
+}
