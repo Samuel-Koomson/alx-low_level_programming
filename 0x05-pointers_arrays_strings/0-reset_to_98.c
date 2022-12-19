d349ec1057d9c2e3b9eafc8da6883f3620e9e6ba
Samuel Koomson <kskoomson1@gmail.com>
2022-12-19 07:14:47 -0800

    pointers taking integer as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * main - function that takes int as parameter
+ * Return : Always 0
+ */
+
+int main()
+{
+	int x = 98;
+	int *ptrx;
+	ptrx = &x
+		_putchar("value of pointer: %p\n", *ptrx);
+	return 0
+}
