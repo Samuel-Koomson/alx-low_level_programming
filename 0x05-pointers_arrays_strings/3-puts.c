96a18376cb32d8a0f821ec1ec3be129ace292aaf
Samuel Koomson <kskoomson1@gmail.com>
2022-12-19 15:00:00 -0800

    printing strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _put - prints a string onto the screen followed by new line
+ * @str: string to be printed
+ * Returns: void
+ */
+
+void _puts(char *str)
+{
+	_putchar("prints a string\n" *str)
+}
