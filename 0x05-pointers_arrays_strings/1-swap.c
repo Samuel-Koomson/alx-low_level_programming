d947850c7bbe2422b1d2b1602e758c6f782d92a6
Samuel Koomson <kskoomson1@gmail.com>
2022-12-19 13:52:23 -0800

    swapping the values of integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,12 +5,13 @@
  * swap_int - this swaps the value of two integers
  * @a: first integer value
  * @b: second integer value
- * Return: n
+ * Return: void
  */
 
 void swap_int(int *a int *b)
 {
 	int k;
+
 	k = *a;
 	*a = *b;
 	*b = k;
