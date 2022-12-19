f7d9e1a9c821af9575146a42805a8b2113349928
Samuel Koomson <kskoomson1@gmail.com>
2022-12-19 12:22:35 -0800

    creating a pointer with int as parameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - function that takes int as parameter
  * Return : Always 0
  */
 
-int main()
+void reset_to_98(int *n)
+
 {
 	int x;
 	x = 98;
