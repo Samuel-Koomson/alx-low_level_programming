0185b94667f41bba091643941e1c4073adc7ed24
Samuel Koomson <kskoomson1@gmail.com>
2022-12-19 23:52:06 -0800

    printing strings in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,15 @@ void print_rev(char *s)
 {
 	int m = 0;
 	int i;
-	while(*s != '\0'
-			{
-			m++;
-			s++;
-			}
-			s--;
-			for (i = m; i > 0; i--)
-			{
-			_putchar(*s);
-			s--;
-			}
+	
+	while(*s != '\0')
+{
+	m++;
+	s++;
+}
+	s--;
+	for (i = m; i > 0; i--)
+{
+	_putchar(*s);
+	s--;
+}
