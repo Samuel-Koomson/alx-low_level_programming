eabe813d4ed2bdf9e0224c1af41fe87481e66baf
Samuel Koomson <kskoomson1@gmail.com>
2022-12-21 15:33:55 -0800

    adding two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+void rev_string(char *n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_line(char *c, int s, int l);
 void print_buffer(char *b, int size);
