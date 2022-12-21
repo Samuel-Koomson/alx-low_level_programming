4f62c35e750e25b74b95cfc6a2998edf9bad8b8f
Samuel Koomson <kskoomson1@gmail.com>
2022-12-21 10:43:03 -0800

    resolving issues with betty

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,13 +3,13 @@
 
 /**
  * _strcat - Function used for the concatenation of two strings
- * @*dest: destination of string
- * @*src: source of string
+ * @dest: destination of string
+ * @src: source of string
  * Return: pointer to the destination string
  */
 
 char *_strcat(char *dest, char *src)
 {
-        (strcat(dest, src));
-	return 0;
+	(strcat(dest, src));
+	return (0);
 }
