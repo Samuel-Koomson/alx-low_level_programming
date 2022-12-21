a74bcb4d1d75bf7002081e6332b89a796ebe4fd7
Samuel Koomson <kskoomson1@gmail.com>
2022-12-21 10:22:44 -0800

    resolving segmentation fault

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * _strncat - concatenates two strings 
+ * @*dest: destination of string
+ * @*src: source string
+ * @n: integer value
+ * Return: pointer to string destination
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	(strncat(dest, src, n));
+	return (0);
+}
