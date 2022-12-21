4809eb71d8d71b6e66e9c855103ba0318ad98563
Samuel Koomson <kskoomson1@gmail.com>
2022-12-21 12:33:18 -0800

    changing lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @char: character
+ * Return: transformed character
+ */
+
+char *string_toupper(char *)
+{
+		int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
