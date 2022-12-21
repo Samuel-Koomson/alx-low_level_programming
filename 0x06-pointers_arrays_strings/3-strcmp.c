b486a2db1b56593444162a582bfccee57f5652fe
Samuel Koomson <kskoomson1@gmail.com>
2022-12-21 11:39:34 -0800

    comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * _strcmp - function compares two strings
+ * @s1: first string variable
+ * @s2: second string variable
+ * Return: compared variable
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return(strcmp(s1, s2));
+}
