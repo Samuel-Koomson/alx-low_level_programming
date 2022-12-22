b1569e1fc0d8c22b18b9e370f172670e9ccf0220
Samuel Koomson <kskoomson1@gmail.com>
2022-12-21 16:23:42 -0800

    adding two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,69 +8,71 @@
 
 void rev_string(char *n)
 {
-	int x = 0;
-	int y = 0;
+	int i = 0;
+	int j = 0;
 	char temp;
 
-	while (*(n + x) != '\0')
+	while (*(n + i) != '\0')
 	{
-		x++;
+		i++;
 	}
-	x--;
+	i--;
 
-	for (y = 0; y < x; y++, x--)
+	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
-		*(n + y) = *(n + x);
-		*(n + x) = temp;
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
 /**
- * infinite_add - used for the addition of 2 numbers 
+ * infinite_add - used for the addition of 2 numbers
  * @n1: first number being addded
  * @n2: second number being added
  * @r: pointer
- * @size_r:  size
+ * @size_r: size
  * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
-	int flower = 0, x = 0, y = 0, digits = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + x) != '\0')
-		x++;
-	while (*(n2 + y) != '\0')
-		y++;
-	x--;
-	y--;
-	if (y >= size_r || x >= size_r)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	while (y >= 0 || x >= 0 || flower == 1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (x < 0)
+		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + x) - '0';
-		if (y < 0)
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + y) - '0';
-		temp_tot = val1 + val2 + flower;
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
-			flower = 1;
+			overflow = 1;
 		else
-			flower = 0;
+			overflow = 0;
 		if (digits >= (size_r - 1))
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
-		y--;
-		x--;
+		j--;
+		i--;
 	}
 	if (digits == size_r)
-		return (0);
+	return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
