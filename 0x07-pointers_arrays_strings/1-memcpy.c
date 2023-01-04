99396e06c6b7c5ec431273b208e6447887b79f68
Samuel Koomson <kskoomson1@gmail.com>
2023-01-03 16:44:01 -0800

    copying memory bytes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,15 +4,15 @@
 * _memcpy - copies bytes from memory area
 * @dest: destination memory
 * @src: source memory
-* @n: unsigned integer
+* @n: number of bytes being copied
 * Return: dest
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
-unsigned int y;
-for (y = 0; a < n; y++)
-dest[y] = src[y];
+unsigned int a;
+for (a = 0; a < n; a++)
+dest[a] = src[a];
 return (dest);
 }
