cd469c1718f4d1781e5714988532d0c65cf8be87
Samuel Koomson <kskoomson1@gmail.com>
2023-01-05 19:33:29 -0800

    corrected variable names

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,9 @@
 int _strlen_recursion(char *s)
 {
 int y;
-i = 0;
+y = 0;
 if (*s == '\0')
 return (0);
-i = 1 + _strlen_recursion(s + 1);
-return (i);
+y = 1 + _strlen_recursion(s + 1);
+return (y);
 }
