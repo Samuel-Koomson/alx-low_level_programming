c3cc1fd2033a175611442ad20f77f0c6b567c7e5
Samuel Koomson <kskoomson1@gmail.com>
2023-01-11 08:54:55 -0800

    function returning pointer to 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width + height < 2 || width < 1 || height < 1)
 		return (NULL);
 
-	alloc = malloc(height * sizeof(*grid));
+	alloc = malloc(height * sizeof(*alloc));
 	if (alloc == NULL)
 		return (NULL);
 
