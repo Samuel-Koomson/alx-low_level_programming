db07630880614dde624ddd2501a035b9d1223fc2
Samuel Koomson <kskoomson1@gmail.com>
2023-01-12 16:09:27 -0800

    correction of incorrect code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,16 +9,16 @@
 * Return: NULL on failure
 */
 
-char *str_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
 	char *sam;
 	unsigned int x = 0, y = 0, length = 0, length1 = 0;
 
-	while (s1 && s1[length])
-		length++;
-	while (s2 && s2[length1])
-		length1++;
+while (s1 && s1[length])
+length++;
+while (s2 && s2[length1])
+length1++;
 
 if
 (n < length1)
