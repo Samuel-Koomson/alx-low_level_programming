89a9c685d71d7e2b1bf85ec80c291bd7be56e016
Samuel Koomson <kskoomson1@gmail.com>
2023-01-12 16:06:15 -0800

    correction of incorrect code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,32 +20,23 @@ char *str_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2 && s2[length1])
 		length1++;
 
-	sam = malloc(sizeof(char) * (length + length1 + 1));
-	if (sam == NULL)
-		return (NULL);
-
-	x = 0;
-	y = 0;
-
-	if (s1)
-	{
-		while (x < length)
-		{
-			sam[x] = s1[x];
-			x++;
-		}
-	}
-
-	if (s2)
-	{
-		while (x < (length + length1))
-		{
-			sam[x] = s2[y];
-			x++;
-			y++;
-		}
-	}
-	sam[x] = '\0';
-
-	return (sam);
+if
+(n < length1)
+sam = malloc(sizeof(char) * (length + n + 1));
+else
+sam = malloc(sizeof(char) * (length + length1 + 1));
+if (!sam)
+return (NULL);
+while (x < length)
+{
+sam[x] = s1[x];
+x++;
+}
+while 
+(n < length1 && x <(length + n))
+sam[x++] = s2[y++];
+while (n >= length1 && x < (length + length1))
+sam[x++] = s2[y++];
+sam[x] = '\0';
+return (sam);
 }
