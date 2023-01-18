542489826ca8e35cd73f5a87787db0ebf8a2efa6
Samuel Koomson <kskoomson1@gmail.com>
2023-01-18 15:09:04 -0800

    betty had issues with my code structure

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
- #include "function_pointer.h"
- #include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
 
- /**
- * print_name - function that prints 
- * @name: character pointer
- * @f: function pointer
- * Return: void
- */
- void print_name(char *name, void (*f)(char *))
+/**
+*print_name - function that prints a name
+*@name: name to be printed
+*@f: function pointer
+*/
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+if (!name || !f)
+return;
+f(name);
 }
