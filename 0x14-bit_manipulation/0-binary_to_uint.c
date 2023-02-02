767ced79d65722c9d7f8464ccb9173abb37fbaf8
Samuel-Koomson <kskoomson1@gmail.com>
2023-02-02 10:47:06 -0800

    correcting the requirements

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,26 +6,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int y;
+	unsigned int c;
 
 	if (b == NULL)
 		return (0);
-	for (y = 0; *b; b++)
+	for (c = 0; *b; b++)
 	{
 		if (*b != '1' && *b != '0')
 		{
 			return (0);
 		}
 		if (*b == '1')
-			y = (y << 1) | 1;
+			c = (c << 1) | 1;
 		else
 			if (*b == '0')
-				y <<= 1;
+				c <<= 1;
 			else
 			{
-				y += 0;
+				c += 0;
 				break;
 			}
 	}
-	return (y);
+	return (c);
 }
