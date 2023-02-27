f75d8cca97b4a5a06ffdbc4f5580a2064e655f1a
Samuel-Koomson <kskoomson1@gmail.com>
2023-02-27 08:56:55 -0800

    resolving betty commenting errors

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,7 +6,7 @@
 * _strpbrk - Function searches a string for any set of bytes
 * @s : pointer string in which char is being searched
 * @accept : pointer string in which char from *s is compared
-* Return : pointer to s if success or NULL if no match is found
+* Return: pointer to s if success or NULL if no match is found
 */
 
 char *_strpbrk(char *s, char *accept)
